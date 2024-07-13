e7bab17c89ded25ea08833398f04456cdbe8c4f7
T-tak2023 <takashi.t1985@icloud.com>
2024-07-13 14:50:21 +0900

    feat: Add profile edit functionality and view for instructors

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -2,4 +2,27 @@ class InstructorsController < ApplicationController
   def profile
     @instructor = current_instructor
   end
+
+  def profile_edit
+    @instructor = current_instructor
+  end
+
+  def profile_update
+    @instructor = current_instructor
+    if @instructor.update(profile_params)
+      redirect_to instructor_profile_path
+    else
+      render "profile_edit"
+    end
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:profile_update, keys: [:instructor_name, :course])
+  end
+
+  def profile_params
+    params.require(:instructor).permit(:instructor_name, :course)
+  end
 end
