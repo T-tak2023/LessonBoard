ff49aed803e8820636b402493f99987417d56613
T-tak2023 <takashi.t1985@icloud.com>
2024-09-08 13:52:19 +0900

    feat: Add validation for start and end times; Improve modal window design - Implemented validation to ensure that the start time is earlier than the end time. - Enhanced modal window design for better user experience.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,14 +3,23 @@ class Lesson < ApplicationRecord
   belongs_to :student
 
   validates :start_time, :end_time, :instructor, :student, :status, presence: true
-end
+  validate :end_time_after_start_time
 
-# rubocop:disable Airbnb/ModuleMethodInWrongFile
-def student_name
-  student.student_name
-end
+   # rubocop:disable Airbnb/ModuleMethodInWrongFile
+  def student_name
+    student.student_name
+  end
 
-def instructor_name
-  instructor.instructor_name
+  def instructor_name
+    instructor.instructor_name
+  end
+  # rubocop:enable Airbnb/ModuleMethodInWrongFile
+
+  private
+
+  def end_time_after_start_time
+    if end_time <= start_time
+      errors.add(:end_time, "は開始時間より後の時刻を選択してください")
+    end
+  end
 end
-# rubocop:enable Airbnb/ModuleMethodInWrongFile
