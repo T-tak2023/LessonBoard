6cd0ccb8c9eb0df4709f8e291e2bda839cb0e167
T-tak2023 <takashi.t1985@icloud.com>
2024-09-29 18:32:42 +0900

    feat: Implement image upload feature for lesson logs; temporary inline adjustment for image display size

--- app/models/lesson_log.rb
+++ app/models/lesson_log.rb
@@ -1,6 +1,9 @@
 class LessonLog < ApplicationRecord
+  mount_uploader :image_material, ImageMaterialUploader
   belongs_to :instructor
   belongs_to :student
+
+  validates :lesson_date, presence: true
 
   def student_name
     student.student_name
