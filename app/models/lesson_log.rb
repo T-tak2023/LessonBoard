a49a650d13c7c6febbd66caa1acf2dce538273ec
T-tak2023 <takashi.t1985@icloud.com>
2024-09-30 21:51:54 +0900

    fix: Update regex to allow query parameters in YouTube URLs

--- app/models/lesson_log.rb
+++ app/models/lesson_log.rb
@@ -5,7 +5,7 @@ class LessonLog < ApplicationRecord
 
   validates :lesson_date, presence: true
   validates :video_material, format: {
-    with: /\A(https:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/|m\.youtube\.com\/watch\?v=)[a-zA-Z0-9_-]{11}(&.*)?\z/,
+    with: /\A(https:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/|m\.youtube\.com\/watch\?v=)[a-zA-Z0-9_-]{11}(\?.*)?\z/,
     message: :invalid_youtube_url,
   }, allow_blank: true
 
