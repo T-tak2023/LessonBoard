6188d86df1911bda93384184455420d1b94f8067
T-tak2023 <takashi.t1985@icloud.com>
2024-11-19 20:13:33 +0900

    fix: Rubocop error

--- app/models/lesson_note.rb
+++ app/models/lesson_note.rb
@@ -4,13 +4,13 @@ class LessonNote < ApplicationRecord
   belongs_to :student, optional: true
 
   validates :lesson_date, presence: true
-  validates :content, :instructor_memo, :student_memo, length: { maximum: 500 }
-  validates :video_material,
-    format: {
-      with: %r{\A(?:https?://)?(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/|m\.youtube\.com/watch\?v=)([a-zA-Z0-9_-]{11})(?:.*)\z},
-      message: :invalid_youtube_url
-    },
-    allow_blank: true
+  validates :content, length: { maximum: 500 }
+  validates :instructor_memo, length: { maximum: 500 }
+  validates :student_memo, length: { maximum: 500 }, allow_blank: true
+  validates :video_material, format: {
+    with: /\A(https:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/|m\.youtube\.com\/watch\?v=)[a-zA-Z0-9_-]{11}(\?.*)?\z/,
+    message: :invalid_youtube_url,
+  }, allow_blank: true
 
   def student_name
     student&.student_name || '情報なし'
