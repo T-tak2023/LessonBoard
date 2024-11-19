54ded4736cbdbad56f5e76cfa7b408d27560ed3a
T-tak2023 <takashi.t1985@icloud.com>
2024-11-19 21:09:02 +0900

    fix: Rubocop error

--- app/controllers/instructors/lesson_notes_controller.rb
+++ app/controllers/instructors/lesson_notes_controller.rb
@@ -72,7 +72,7 @@ class Instructors::LessonNotesController < ApplicationController
     patterns = [
       /youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})/,
       /youtu\.be\/([a-zA-Z0-9_-]{11})/,
-      /m\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})/
+      /m\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})/,
     ]
 
     patterns.each do |pattern|
