c3091f00af27041debbcd96594202b5ce21818b0
T-tak2023 <takashi.t1985@icloud.com>
2024-12-27 22:42:18 +0900

    test: Add basic validations, associations, and callback tests for Student model

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'バリデーション' do
+    it { should validate_presence_of(:student_name) }
+    it { should validate_length_of(:student_name).is_at_most(20) }
+    it { should validate_length_of(:course).is_at_most(50) }
+  end
+
+  context 'アソシエーション' do
+    it { should belong_to(:instructor) }
+    it { should have_many(:lessons).dependent(:destroy) }
+    it { should have_many(:lesson_notes) }
+  end
+
+  context 'コールバック' do
+    it '新規登録時に enrollment_date が自動設定されること' do
+      student = build(:student, enrollment_date: nil)
+      student.save
+      expect(student.enrollment_date).to eq(Date.today)
+    end
+  end
 end
