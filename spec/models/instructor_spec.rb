38aca5a1b3a44551713183c5eb5bf91d1b98987a
T-tak2023 <takashi.t1985@icloud.com>
2024-12-28 16:37:26 +0900

    refactor: Update Instructor model tests by removing redundant tests and adding contexts for validation and associations

--- spec/models/instructor_spec.rb
+++ spec/models/instructor_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Instructor, type: :model do
   context 'バリデーション' do
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password) }
     it { should validate_presence_of(:instructor_name) }
     it { should validate_length_of(:instructor_name).is_at_most(20) }
     it { should validate_length_of(:course).is_at_most(50) }
+  end
+
+  context 'アソシエーション' do
     it { should have_many(:students) }
     it { should have_many(:lessons) }
   end
