cfe19de7779f09c066d405225de72fdd4623d38d
T-tak2023 <takashi.t1985@icloud.com>
2025-01-07 15:30:57 +0900

    feat: Update Student model specs with let and dependent nullify tests

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -10,20 +10,28 @@ RSpec.describe Student, type: :model do
   describe 'アソシエーション' do
     it { should belong_to(:instructor) }
     it { should have_many(:lessons).dependent(:destroy) }
-    it { should have_many(:lesson_notes) }
+    it { should have_many(:lesson_notes).dependent(:nullify) }
 
     context 'student を削除した時' do
+      let(:student) { create(:student) }
+      let!(:lesson) { create(:lesson, student: student) }
+      let!(:lesson_note) { create(:lesson_note, student: student) }
+
       it '関連する lesson も削除されること' do
-        student = create(:student)
-        create(:lesson, student: student)
         expect { student.destroy }.to change { Lesson.count }.by(-1)
+      end
+
+      it '関連する lesson_notes の student_id が null になること' do
+        student.destroy
+        expect(lesson_note.reload.student_id).to be_nil
       end
     end
   end
 
   describe 'コールバック' do
+    let(:student) { build(:student, enrollment_date: nil) }
+
     it '新規登録時に enrollment_date が自動設定されること' do
-      student = build(:student, enrollment_date: nil)
       student.save
       expect(student.enrollment_date).to eq(Date.today)
     end
